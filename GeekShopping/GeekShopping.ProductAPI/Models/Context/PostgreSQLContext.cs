905374a0bd0c6d9a88d3a4591835fd3e6ade593c
Eduardo Felipe <edufslima9@gmail.com>
2022-11-03 23:12:38 -0300

    build: development of ProductAPI architecture

---
+++ GeekShopping/GeekShopping.ProductAPI/Models/Context/PostgreSQLContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.ProductAPI.Models.Context
+{
+    public class PostgreSQLContext : DbContext
+    {
+        public PostgreSQLContext() {}
+        public PostgreSQLContext(DbContextOptions<PostgreSQLContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+    }
+}
--- GeekShopping/GeekShopping.ProductAPI/Program.cs
+++ GeekShopping/GeekShopping.ProductAPI/Program.cs
@@ -1,6 +1,21 @@
+using AutoMapper;
+using GeekShopping.ProductAPI.Config;
+using GeekShopping.ProductAPI.Models.Context;
+using GeekShopping.ProductAPI.Repository;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+var connection = builder.Configuration["PostgreSQLConnection:PostgreSQLConnectionString"];
+
+builder.Services.AddDbContext<PostgreSQLContext>(options => options.UseNpgsql(connection));
+
+IMapper mapper = MappingConfig.RegisterMapps().CreateMapper();
+builder.Services.AddSingleton(mapper);
+builder.Services.AddAutoMapper(AppDomain.CurrentDomain.GetAssemblies());
+
+builder.Services.AddScoped<IProductRepository, ProductRepository>();
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
