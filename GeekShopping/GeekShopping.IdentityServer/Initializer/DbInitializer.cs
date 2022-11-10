cebd4eed556889a7db8e99180feaf722dd0b49f4
Eduardo Felipe <edufslima9@gmail.com>
2022-11-09 22:01:29 -0300

    build: creating Identity Server

---
+++ GeekShopping/GeekShopping.IdentityServer/Initializer/DbInitializer.cs
@@ -0,0 +1,73 @@
+﻿using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model;
+using GeekShopping.IdentityServer.Model.Context;
+using IdentityModel;
+using Microsoft.AspNetCore.Identity;
+using System.Security.Claims;
+
+namespace GeekShopping.IdentityServer.Initializer
+{
+    public class DbInitializer : IDbInitializer
+    {
+        private readonly PostgreSQLContext _context;
+        private readonly UserManager<ApplicationUser> _user;
+        private readonly RoleManager<IdentityRole> _role;
+
+        public DbInitializer(PostgreSQLContext context, UserManager<ApplicationUser> user, RoleManager<IdentityRole> role)
+        {
+            _context = context;
+            _user = user;
+            _role = role;
+        }
+
+        public void Initialize()
+        {
+            if (_role.FindByNameAsync(IdentityConfiguration.Admin).Result != null) return;
+
+            _role.CreateAsync(new IdentityRole(IdentityConfiguration.Admin)).GetAwaiter().GetResult();
+            _role.CreateAsync(new IdentityRole(IdentityConfiguration.Customer)).GetAwaiter().GetResult();
+
+            ApplicationUser admin = new ApplicationUser()
+            {
+                UserName = "eduardo-admin",
+                Email = "eduardlimmadev@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "+55 (81) 91234-5678",
+                FirstName = "Eduardo",
+                LastName = "Admin"
+            };
+
+            _user.CreateAsync(admin, "Ed123$").GetAwaiter().GetResult();
+            _user.AddToRoleAsync(admin, IdentityConfiguration.Admin).GetAwaiter().GetResult();
+
+            var adminClaims = _user.AddClaimsAsync(admin, new Claim[]
+            {
+                new Claim(JwtClaimTypes.Name, $"{admin.FirstName} {admin.LastName}"),
+                new Claim(JwtClaimTypes.GivenName, admin.FirstName),
+                new Claim(JwtClaimTypes.FamilyName, admin.LastName),
+                new Claim(JwtClaimTypes.Role, IdentityConfiguration.Admin),
+            }).Result;
+
+            ApplicationUser customer = new ApplicationUser()
+            {
+                UserName = "eduardo-customer",
+                Email = "edufslima9@gmail.com",
+                EmailConfirmed = true,
+                PhoneNumber = "+55 (81) 91234-5678",
+                FirstName = "Eduardo",
+                LastName = "Customer"
+            };
+
+            _user.CreateAsync(customer, "Ed123$").GetAwaiter().GetResult();
+            _user.AddToRoleAsync(customer, IdentityConfiguration.Customer).GetAwaiter().GetResult();
+
+            var customerClaims = _user.AddClaimsAsync(customer, new Claim[]
+            {
+                new Claim(JwtClaimTypes.Name, $"{customer.FirstName} {customer.LastName}"),
+                new Claim(JwtClaimTypes.GivenName, customer.FirstName),
+                new Claim(JwtClaimTypes.FamilyName, customer.LastName),
+                new Claim(JwtClaimTypes.Role, IdentityConfiguration.Customer),
+            }).Result;
+        }
+    }
+}
---
+++ GeekShopping/GeekShopping.IdentityServer/Program.cs
@@ -0,0 +1,63 @@
+using GeekShopping.IdentityServer.Configuration;
+using GeekShopping.IdentityServer.Model.Context;
+using GeekShopping.IdentityServer.Model;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using GeekShopping.IdentityServer.Initializer;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddRazorPages();
+
+var connection = builder.Configuration["PostgreSQLConnection:PostgreSQLConnectionString"];
+
+builder.Services.AddDbContext<PostgreSQLContext>(options => options.UseNpgsql(connection));
+
+builder.Services.AddIdentity<ApplicationUser, IdentityRole>()
+    .AddEntityFrameworkStores<PostgreSQLContext>()
+    .AddDefaultTokenProviders();
+
+var buildIdentity = builder.Services.AddIdentityServer(options =>
+{
+    options.Events.RaiseErrorEvents = true;
+    options.Events.RaiseInformationEvents = true;
+    options.Events.RaiseFailureEvents = true;
+    options.Events.RaiseSuccessEvents = true;
+    options.EmitStaticAudienceClaim = true;
+})
+.AddInMemoryIdentityResources(IdentityConfiguration.IdentityResources)
+.AddInMemoryApiScopes(IdentityConfiguration.ApiScopes)
+.AddInMemoryClients(IdentityConfiguration.Clients)
+.AddAspNetIdentity<ApplicationUser>();
+
+builder.Services.AddScoped<IDbInitializer, DbInitializer>();
+
+buildIdentity.AddDeveloperSigningCredential();
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseRouting();
+
+app.UseIdentityServer();
+
+app.UseAuthorization();
+
+using var scope = app.Services.CreateScope();
+var dbService = scope.ServiceProvider.GetRequiredService<IDbInitializer>();
+
+dbService.Initialize();
+
+app.MapRazorPages();
+
+app.Run();
