3808631962e6f555922440d984239769aed93e50
Eduardo Felipe <edufslima9@gmail.com>
2023-06-26 21:31:59 -0300

    build: adding basic configuration in GeekShopping.CartApi service

---
+++ GeekShopping/GeekShopping.CartAPI/Models/Context/PostgreSQLContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CartAPI.Models.Context
+{
+    public class PostgreSQLContext : DbContext
+    {
+        public PostgreSQLContext() { }
+        public PostgreSQLContext(DbContextOptions<PostgreSQLContext> options) : base(options) { }
+
+        public DbSet<Product> Products { get; set; }
+
+    }
+}
