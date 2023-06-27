4c92123ed049078136b45ba6229a7db1843b8977
Eduardo Felipe <edufslima9@gmail.com>
2023-06-26 22:10:01 -0300

    [CartAPI] build: adding cart repository

---
+++ GeekShopping/GeekShopping.CartAPI/Repository/CartRepository.cs
@@ -0,0 +1,51 @@
+﻿using AutoMapper;
+using GeekShopping.CartAPI.Data.ValueObjects;
+using GeekShopping.CartAPI.Models;
+using GeekShopping.CartAPI.Models.Context;
+using GeekShopping.CartAPI.Repository.Interface;
+using Microsoft.EntityFrameworkCore;
+
+namespace GeekShopping.CartAPI.Repository
+{
+    public class CartRepository : ICartRepository
+    {
+        private readonly PostgreSQLContext _context;
+        private IMapper _mapper;
+
+        public CartRepository(PostgreSQLContext context, IMapper mapper)
+        {
+            _context = context;
+            _mapper = mapper;
+        }
+
+        public Task<bool> ApplyCoupom(string userId, string coupomCode)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ClearCart(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartVO> FindCartByUserId(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveCoupom(string userId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveFromCart(long cartDetailsId)
+        {
+            throw new NotImplementedException();
+        }
+
+        public async Task<CartVO> SaveOrUpdateCart(CartVO cartVO)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ GeekShopping/GeekShopping.CartAPI/Repository/Interface/ICartRepository.cs
@@ -0,0 +1,14 @@
+﻿using GeekShopping.CartAPI.Data.ValueObjects;
+
+namespace GeekShopping.CartAPI.Repository.Interface
+{
+    public interface ICartRepository
+    {
+        Task<CartVO> FindCartByUserId(string userId);
+        Task<CartVO> SaveOrUpdateCart(CartVO cart);
+        Task<bool> RemoveFromCart(long cartDetailsId);
+        Task<bool> ApplyCoupom(string userId, string coupomCode);
+        Task<bool> RemoveCoupom(string userId);
+        Task<bool> ClearCart(string userId);
+    }
+}
