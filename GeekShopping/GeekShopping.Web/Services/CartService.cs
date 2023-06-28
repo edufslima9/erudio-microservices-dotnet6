4183f7cd3adef607a7ded8ccbd793e9d355d67a7
Eduardo Felipe <edufslima9@gmail.com>
2023-06-28 13:37:33 -0300

    [Web] - build: configuring models and building service from consumes CartAPI Service

---
+++ GeekShopping/GeekShopping.Web/Services/CartService.cs
@@ -0,0 +1,48 @@
+﻿using GeekShopping.Web.Models;
+using GeekShopping.Web.Services.IServices;
+
+namespace GeekShopping.Web.Services
+{
+    public class CartService : ICartService
+    {
+        public Task<CartViewModel> AddItemToCart(CartViewModel cart, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ApplyCoupom(CartViewModel cart, string couponCode, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartViewModel> Checkout(CartHeaderViewModel cartHeader, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> ClearCart(string userId, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartViewModel> FindCartByUserId(string userId, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveCoupom(string userId, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<bool> RemoveFromCard(long cartId, string token)
+        {
+            throw new NotImplementedException();
+        }
+
+        public Task<CartViewModel> UpdateCart(CartViewModel cart, string token)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
