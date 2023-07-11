1e30fb8e3772f73eb15976c08cd7fae574f91ec6
Eduardo Felipe <edufslima9@gmail.com>
2023-07-11 01:05:49 -0300

    [OrderAPI] - fix: adding jwt secutiry configuration

--- GeekShopping/GeekShopping.OrderAPI/Program.cs
+++ GeekShopping/GeekShopping.OrderAPI/Program.cs
@@ -3,6 +3,7 @@ using GeekShopping.OrderAPI.RabbitMQConsumer;
 using GeekShopping.OrderAPI.RabbitMQSender;
 using GeekShopping.OrderAPI.Repository;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using Microsoft.OpenApi.Models;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -21,6 +22,25 @@ builder.Services.AddHostedService<RabbitMQCheckoutConsumer>();
 builder.Services.AddSingleton<IRabbitMQMessageSender, RabbitMQMessageSender>();
 
 builder.Services.AddControllers();
+
+builder.Services.AddAuthentication("Bearer")
+    .AddJwtBearer("Bearer", options =>
+    {
+        options.Authority = "https://localhost:4435/";
+        options.TokenValidationParameters = new TokenValidationParameters
+        {
+            ValidateAudience = false
+        };
+    });
+
+builder.Services.AddAuthorization(options =>
+{
+    options.AddPolicy("ApiScope", policy =>
+    {
+        policy.RequireAuthenticatedUser();
+        policy.RequireClaim("scope", "geek_shopping");
+    });
+});
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen(c =>
